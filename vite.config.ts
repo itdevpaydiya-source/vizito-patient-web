323eb451c27d47152767944a9ed680df47609093
Ravi Teja <batturaviteja18@gmail.com>
2026-09-09 21:13:38 +0530

    vite config file update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5174
+    port: 5174,
+    allowedHosts: ['vizito.in', '.vizito.in', 'localhost']
   },
 })
 
